9e1fa7a8395a8314a2c3b5df33b711c9709d95f5
Brittani <brittaniluce@gmail.com>
2017-01-31 22:18:55 -0600

    adjust error wording

--- main.py
+++ main.py
@@ -18,45 +18,38 @@ import webapp2
 import cgi
 import re
 
-def build_head():  # Change style type in head to css when doing finish work
-    """Creates the html header for the page."""
-    page_header = """
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <meta charset='utf-8'>
-        <link type="text/css" rel="stylesheet" href="normalize.css">
-        <link type="text/css" rel="stylesheet" href="styles.css">
-        <title>User Sign-Up</title>
-        <style type="text/css">
-            .error {
-                color: red;
-            }
-        </style>
-    </head>
-    <body>
-        <h1>
-            <a href="/">User Sign-Up</a>
-        </h1>
-    """
-    return page_header
-
-def build_footer():
-    """Creates the html footer for the page."""
-    page_footer = """
-    </body>
-    <footer>
-        <address>
-            <p><br>Contents written by: <b>Brittani Luce</b></p>
-            <p>Written for: LaunchCode 101 -- User Sign-Up
-            <p>Thank you for visiting!</p>
-        </address>
-            <p>&#169;Brittani Luce, 2017
-            <br><br></p>
-    </footer>
-    </html>
-    """
-    return page_footer
+page_header = """
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <link type="text/css" rel="stylesheet" href="normalize.css">
+    <link type="text/css" rel="stylesheet" href="styles.css">
+    <title>User Sign-Up</title>
+    <style type="text/css">
+        .error {
+            color: red;
+        }
+    </style>
+</head>
+<body>
+    <h1>
+        <a href="/">User Sign-Up</a>
+    </h1>
+"""
+page_footer = """
+</body>
+<footer>
+    <address>
+        <p><br>Contents written by: <b>Brittani Luce</b></p>
+        <p>Written for: LaunchCode 101 -- User Sign-Up
+        <p>Thank you for visiting!</p>
+    </address>
+        <p>&#169;Brittani Luce, 2017
+        <br><br></p>
+</footer>
+</html>
+"""
 
 def valid_username(user_input):
     """Checks if the username input is valid."""
@@ -87,57 +80,122 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         """First page that the user comes to. Handles the first GET request."""
         # sets up the Username field
-        user_label = "<form action='sign-up' method='post'><label>Username: </label>"
+        user_content = "" # initialize user content
+        user_label = "<form action='/sign-up' method='post'><label>Username: </label>"
         user_input = "<input type='text' name='user'>" + user_content + "</input>"
-        user = "<p>" + user_label + user_input + "</p>"
+
+        error1 = self.request.get("no_username_error")
+        error1_element = "<span class='error'>" + error1 + "</span>"
+
+        error2 = self.request.get("space_error")
+        error2_element = "<span class='error'>" + error1 + "</span>"
+
+        error3 = self.request.get("invalid_user")
+        error3_element = "<span class='error'>" + error3 + "</span>"
+
+        user = "<p>" + user_label + user_input + "  " + error1_element + error2_element + error3_element + "</p>"
 
         # sets up the password field
-        password_label = "label>Password: </label>"
+        password_label = "<label>Password: </label>"
         password_input = "<input type='password' name='password'/>"
-        password = "<p>" + password_label + password_input + "</p>"
+
+        error4 = self.request.get("no_password_error")
+        error4_element = "<span class='error'>" + error4 + "</span>"
+
+        error5 = self.request.get("invalid_password")
+        error5_element = "<span class='error'>" + error5 + "</span>"
+
+        password = "<p>" + password_label + password_input + "  " + error4_element + error5_element + "</p>"
 
         # sets up the verify password field
         verify_label = "<label>Verify Password: </label>"
         verify_input = "<input type='password' name='verify'/>"
-        verify = "<p>" + verify_label + verify_input + "</p>"
+
+        error6 = self.request.get("pass_not_verified")
+        error6_element = "<span class='error'>" + error6 + "</span>"
+
+        verify = "<p>" + verify_label + verify_input + "  " + error6_element + "</p>"
 
         # sets up the optional email field
+        email_content = "" # initialize email content
         email_label = "<label>Email (optional): </label>"
         email_input = "<input type='text' name='email'>" + email_content + "</input>"
-        email = "<p>" + email_label + email_input + "</p>"
+
+        error7 = self.request.get("space_in_email")
+        error7_element = "<span class='error'>" + error7 + "</span>"
+
+        error8 = self.request.get("invalid_email")
+        error8_element = "<span class='error'>" + error8 + "</span>"
+
+        email = "<p>" + email_label + email_input + "  " + error7_element + error8_element + "</p>"
 
         # sets up the submit button
         submit = "<p><input type='submit' value='submit'/></form></p>"
 
-        #ERRORS GO HERE
-        error1 = self.request.get("no_username_error")
-        error1_element = "<span class='error'>" + error1 + "</span>" if error1 else ""
-
-        error2 = self.request.get("space_error")
-        error2_element = "<span class='error'>" + error1 + "</span>" if error2 else ""
-
-        error3 = self.request.get("invalid_user")
-        error3_element = "<span class='error'>" + error3 + "</span>" if error3 else ""
-
-        error4 = self.request.get("no_password_error")
-        error4_element = "<span class='error'>" + error4 + "</span>" if error4 else ""
-
-        error5 = self.request.get("invalid_password")
-        error5_element = "<span class='error'>" + error5 + "</span>" if error5 else ""
-
-        error6 = self.request.get("pass_not_verified")
-        error6_element = "<span class='error'>" + error6 + "</span>" if error6 else ""
-
-        error7 = self.request.get("space_in_email")
-        error7_element = "<span class='error'>" + error7 + "</span>" if error7 else ""
-
-        error8 = self.request.get("invalid_email")
-        error8_element = "<span class='error'>" + error8 + "</span>" if error8 else ""
-
-        content = build_head() + user + error1_element + error2_element + error3_element + password + error4_element + error5_element + verify + error6_element + email + error7_element + error8_element + submit + build_footer
+        content = page_header + user + password + verify + email + submit + page_footer
+        self.response.write(content)
 
 class SignUp(webapp2.RequestHandler):
     """Handles requests coming into '/sign-up'."""
+    def get(self):
+        # sets up the Username field
+        user_content = "" # initialize user content
+        user_label = "<form action='/sign-up' method='post'><label>Username: </label>"
+        user_input = "<input type='text' name='user'>" + user_content + "</input>"
+        user_content = user_input
+
+        error1 = self.request.get("no_username_error")
+        error1_element = "<span class='error'>" + error1 + "</span>"
+
+        error2 = self.request.get("space_error")
+        error2_element = "<span class='error'>" + error1 + "</span>"
+
+        error3 = self.request.get("invalid_user")
+        error3_element = "<span class='error'>" + error3 + "</span>"
+
+        user = "<p>" + user_label + user_input + "  " + error1_element + error2_element + error3_element + "</p>"
+
+        # sets up the password field
+        password_label = "<label>Password: </label>"
+        password_input = "<input type='password' name='password'/>"
+
+        error4 = self.request.get("no_password_error")
+        error4_element = "<span class='error'>" + error4 + "</span>"
+
+        error5 = self.request.get("invalid_password")
+        error5_element = "<span class='error'>" + error5 + "</span>"
+
+        password = "<p>" + password_label + password_input + "  " + error4_element + error5_element + "</p>"
+
+        # sets up the verify password field
+        verify_label = "<label>Verify Password: </label>"
+        verify_input = "<input type='password' name='verify'/>"
+
+        error6 = self.request.get("pass_not_verified")
+        error6_element = "<span class='error'>" + error6 + "</span>"
+
+        verify = "<p>" + verify_label + verify_input + "  " + error6_element + "</p>"
+
+        # sets up the optional email field
+        email_content = "" # initialize email content
+        email_label = "<label>Email (optional): </label>"
+        email_input = "<input type='text' name='email'>" + email_content + "</input>"
+        email_content = email_input
+
+        error7 = self.request.get("space_in_email")
+        error7_element = "<span class='error'>" + error7 + "</span>"
+
+        error8 = self.request.get("invalid_email")
+        error8_element = "<span class='error'>" + error8 + "</span>"
+
+        email = "<p>" + email_label + email_input + "  " + error7_element + error8_element + "</p>"
+
+        # sets up the submit button
+        submit = "<p><input type='submit' value='submit'/></form></p>"
+
+        content = page_header + user + password + verify + email + submit + page_footer
+        self.response.write(content)
+
     def post(self):
         # set the validity values to False
         user_valid = False
@@ -150,13 +208,13 @@ class SignUp(webapp2.RequestHandler):
         # check username validity
         if user_escape == "":#error1
             no_username_error = "A username is required.".format(user_escape)
-            self.redirect("/sign-up" + no_username_error)
+            self.redirect("/sign-up/no_username_error=" + no_username_error)
         elif " " in user_escape:#error2
             space_error = "Username cannot contain spaces.".format(user_escape)
-            self.redirect("/sign-up" + space_error)
+            self.redirect("/sign-up/?space_error=" + space_error)
         elif not valid_username(user_escape):#error3
             invalid_user = "Username must contain between 3 and 20 characters (Upper, lower, _, -).".format(user_escape)
-            self.redirect("/sign-up" + invalid_user)
+            self.redirect("/sign-up/?invalid_user=" + invalid_user)
         else:#valid
             user_valid = True
 
@@ -165,7 +223,7 @@ class SignUp(webapp2.RequestHandler):
         # check password validity
         if password == "":#error4
             no_password_error = "A password is required.".format(password)
-            self.redirect("/sign-up)
+            self.redirect("/sign-up")
         elif not valid_password(password):#error5
             invalid_password = "Password contains invalid characters. Please try again."
             self.redirect("/sign-up")
@@ -190,6 +248,7 @@ class SignUp(webapp2.RequestHandler):
         if user_valid and pass_valid and email_valid:#if all valid
             self.redirect("/welcome")
 
+
 class Welcome(webapp2.RequestHandler):
     """Handles requests coming into '/welcome'."""
     def post(self):
@@ -197,7 +256,8 @@ class Welcome(webapp2.RequestHandler):
         email = self.request.get(email_input)
         welcome = "Hello %s, thank you for signing up! We've added your email address as %s. Thank you!" % (user, email)
         welcome_element = "<p class='welcome'>" + welcome + "</p>"
-        content = build_head() + welcome_element + build_footer()
+        welcome_content = page_header + welcome_element + page_footer
+        self.response.write(content)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
