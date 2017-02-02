cde4aa21d33634825c4efe7fb539cfb001e877bf
Brittani <brittaniluce@gmail.com>
2017-02-01 23:08:26 -0600

    added css folder...reworked entire format to get working...began css formatting

--- main.py
+++ main.py
@@ -23,25 +23,34 @@ page_header = """
 <html>
 <head>
     <meta charset='utf-8'>
-    <link type="text/css" rel="stylesheet" href="normalize.css">
-    <link type="text/css" rel="stylesheet" href="styles.css">
+    <link type="text/css" rel="stylesheet" href="css/normalize.css">
+    <link type="text/css" rel="stylesheet" href="css/styles.css">
     <title>User Sign-Up</title>
-    <style type="text/css">
-        .error {
-            color: red;
-        }
-    </style>
 </head>
 <body>
     <h1>
         <a href="/">User Sign-Up</a>
     </h1>
 """
+page_header_welcome = """
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <link type="text/css" rel="stylesheet" href="normalize.css">
+    <link type="text/css" rel="stylesheet" href="styles_wel.css">
+    <title>Welcome!</title>
+</head>
+<body>
+    <h1>
+        Welcome!
+    </h1>
+"""
 page_footer = """
 </body>
 <footer>
     <address>
-        <p><br>Contents written by: <b>Brittani Luce</b></p>
+        <p><br>Contents written by: <span id='myname'<b>Brittani Luce</b></span></p>
         <p>Written for: LaunchCode 101 -- User Sign-Up
         <p>Thank you for visiting!</p>
     </address>
@@ -50,6 +59,34 @@ page_footer = """
 </footer>
 </html>
 """
+
+def build_form(username='', user_error='', password_error='', verify_password='', email='', email_error=''):
+    return """
+        <div>
+        <form method='post'>
+        	<p><label>*Username*: </label>
+        	<input type='text' name='user' value={username}></input></p>
+        	<p><span class="error">{user_error}</span></p>
+
+        	<p><label>*Password*: </label>
+        	<input type='password' name='password'/></p>
+        	<p><span class="error">{password_error}</span></p>
+
+        	<p><label>*Verify Password*: </label>
+        	<input type='password' name='verify'/></p>
+        	<p><span class="error">{verify_password}</span></p>
+
+        	<p><label>Email: </label>
+        	<input type='text' name='email' value={email}></input></p>
+        	<p><span class="error">{email_error}</span></p>
+            <br>
+            <br>
+            <p><span id='req'>*Required Information*</span></p>
+        	<p><input type='submit' value='Let&apos;s&nbsp;Go!'/></p>
+        </form>
+        </div>
+        """.format(username=username, user_error=user_error, password_error=password_error,
+            verify_password=verify_password, email=email, email_error=email_error)
 
 def valid_username(user_input):
     """Checks if the username input is valid."""
@@ -64,7 +101,7 @@ def valid_password(password_input):
 def valid_email(email_input):
     """Checks if ther is email input. If there is, checks if it is valid."""
     if email_input == "":
-        pass
+        return True
     else:
         email_re = re.compile(r"^[\S]+@[\S]+.[\S]+$")
         return email_re.match(email_input)
@@ -72,195 +109,76 @@ def valid_email(email_input):
 def password_verified(password_input, verify_input):
     """Checks if the Password and Verify Password fields match."""
     if password_input == verify_input:
-        pass
+        return True
     else:
         return False
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         """First page that the user comes to. Handles the first GET request."""
-        # sets up the Username field
-        user_content = "" # initialize user content
-        user_label = "<form action='/sign-up' method='post'><label>Username: </label>"
-        user_input = "<input type='text' name='user'>" + user_content + "</input>"
-
-        error1 = self.request.get("no_username_error")
-        error1_element = "<span class='error'>" + error1 + "</span>"
-
-        error2 = self.request.get("space_error")
-        error2_element = "<span class='error'>" + error1 + "</span>"
-
-        error3 = self.request.get("invalid_user")
-        error3_element = "<span class='error'>" + error3 + "</span>"
-
-        user = "<p>" + user_label + user_input + "  " + error1_element + error2_element + error3_element + "</p>"
-
-        # sets up the password field
-        password_label = "<label>Password: </label>"
-        password_input = "<input type='password' name='password'/>"
-
-        error4 = self.request.get("no_password_error")
-        error4_element = "<span class='error'>" + error4 + "</span>"
-
-        error5 = self.request.get("invalid_password")
-        error5_element = "<span class='error'>" + error5 + "</span>"
-
-        password = "<p>" + password_label + password_input + "  " + error4_element + error5_element + "</p>"
-
-        # sets up the verify password field
-        verify_label = "<label>Verify Password: </label>"
-        verify_input = "<input type='password' name='verify'/>"
-
-        error6 = self.request.get("pass_not_verified")
-        error6_element = "<span class='error'>" + error6 + "</span>"
-
-        verify = "<p>" + verify_label + verify_input + "  " + error6_element + "</p>"
-
-        # sets up the optional email field
-        email_content = "" # initialize email content
-        email_label = "<label>Email (optional): </label>"
-        email_input = "<input type='text' name='email'>" + email_content + "</input>"
-
-        error7 = self.request.get("space_in_email")
-        error7_element = "<span class='error'>" + error7 + "</span>"
-
-        error8 = self.request.get("invalid_email")
-        error8_element = "<span class='error'>" + error8 + "</span>"
-
-        email = "<p>" + email_label + email_input + "  " + error7_element + error8_element + "</p>"
-
-        # sets up the submit button
-        submit = "<p><input type='submit' value='submit'/></form></p>"
-
-        content = page_header + user + password + verify + email + submit + page_footer
-        self.response.write(content)
-
-class SignUp(webapp2.RequestHandler):
-    """Handles requests coming into '/sign-up'."""
-    def get(self):
-        # sets up the Username field
-        user_content = "" # initialize user content
-        user_label = "<form action='/sign-up' method='post'><label>Username: </label>"
-        user_input = "<input type='text' name='user'>" + user_content + "</input>"
-        user_content = user_input
-
-        error1 = self.request.get("no_username_error")
-        error1_element = "<span class='error'>" + error1 + "</span>"
-
-        error2 = self.request.get("space_error")
-        error2_element = "<span class='error'>" + error1 + "</span>"
-
-        error3 = self.request.get("invalid_user")
-        error3_element = "<span class='error'>" + error3 + "</span>"
-
-        user = "<p>" + user_label + user_input + "  " + error1_element + error2_element + error3_element + "</p>"
-
-        # sets up the password field
-        password_label = "<label>Password: </label>"
-        password_input = "<input type='password' name='password'/>"
-
-        error4 = self.request.get("no_password_error")
-        error4_element = "<span class='error'>" + error4 + "</span>"
-
-        error5 = self.request.get("invalid_password")
-        error5_element = "<span class='error'>" + error5 + "</span>"
-
-        password = "<p>" + password_label + password_input + "  " + error4_element + error5_element + "</p>"
-
-        # sets up the verify password field
-        verify_label = "<label>Verify Password: </label>"
-        verify_input = "<input type='password' name='verify'/>"
-
-        error6 = self.request.get("pass_not_verified")
-        error6_element = "<span class='error'>" + error6 + "</span>"
-
-        verify = "<p>" + verify_label + verify_input + "  " + error6_element + "</p>"
-
-        # sets up the optional email field
-        email_content = "" # initialize email content
-        email_label = "<label>Email (optional): </label>"
-        email_input = "<input type='text' name='email'>" + email_content + "</input>"
-        email_content = email_input
-
-        error7 = self.request.get("space_in_email")
-        error7_element = "<span class='error'>" + error7 + "</span>"
-
-        error8 = self.request.get("invalid_email")
-        error8_element = "<span class='error'>" + error8 + "</span>"
-
-        email = "<p>" + email_label + email_input + "  " + error7_element + error8_element + "</p>"
-
-        # sets up the submit button
-        submit = "<p><input type='submit' value='submit'/></form></p>"
-
-        content = page_header + user + password + verify + email + submit + page_footer
-        self.response.write(content)
+        self.response.write(page_header + build_form() + page_footer)
 
     def post(self):
-        # set the validity values to False
-        user_valid = False
-        pass_valid = False
-        email_valid = False
+        username = self.request.get("user")
+        user_esc = cgi.escape(username, quote=True)
+        password = self.request.get("password")
+        verify = self.request.get("verify")
+        email = self.request.get("email")
+        fail = False
 
-        # pull in username
-        username = self.request.get("user_input")
-        user_escape = cgi.escape(username, quote=True)
-        # check username validity
-        if user_escape == "":#error1
-            no_username_error = "A username is required.".format(user_escape)
-            self.redirect("/sign-up/no_username_error=" + no_username_error)
-        elif " " in user_escape:#error2
-            space_error = "Username cannot contain spaces.".format(user_escape)
-            self.redirect("/sign-up/?space_error=" + space_error)
-        elif not valid_username(user_escape):#error3
-            invalid_user = "Username must contain between 3 and 20 characters (Upper, lower, _, -).".format(user_escape)
-            self.redirect("/sign-up/?invalid_user=" + invalid_user)
-        else:#valid
-            user_valid = True
+        if user_esc == "":
+            username_error = "You must enter a Username!"
+            fail = True
+        elif " " in user_esc:
+            username_error = "Username cannot contain spaces!"
+            fail = True
+        elif not valid_username(user_esc):
+            username_error = "Please enter a valid Username!"
+            fail = True
+        else:
+            username_error = ""
 
-        # pull in password
-        password = self.request.get("password_input")
-        # check password validity
-        if password == "":#error4
-            no_password_error = "A password is required.".format(password)
-            self.redirect("/sign-up")
-        elif not valid_password(password):#error5
-            invalid_password = "Password contains invalid characters. Please try again."
-            self.redirect("/sign-up")
-        elif not password_verified(password):#error6
-            pass_not_verified = "The password fields do not match."
-            self.redirect("/sign-up")
-        else:#valid
-            pass_valid = True
+        if not valid_password(password):
+            password_error = "Please enter a valid Password!"
+            fail = True
+        else:
+            password_error = ""
 
-        # pull in email
-        email = self.request.get("email_input")
-        # check email validity
-        if " " in email:#error7
-            space_in_email = "Email cannot contain spaces."
-            self.redirect("/sign-up")
-        elif not valid_email(email):#error8
-            invalid_email = "email is not valid, it must be in this form:  abc@something.com...Please try again."
-            self.redirect("/sign-up")
-        else:#valid
-            email_valid = True
+        if not password_verified(password, verify):
+            verify_error = "Password fields must match!"
+            fail = True
+        else:
+            verify_error = ""
 
-        if user_valid and pass_valid and email_valid:#if all valid
-            self.redirect("/welcome")
+        if not valid_email(email):
+            email_error = "Please enter a valid Email!"
+            fail = True
+        else:
+            email_error = ""
+
+        if fail:
+            self.response.write(page_header + build_form(username=user_esc, user_error=username_error,
+            password_error=password_error, verify_password=verify_error, email=email,
+            email_error=email_error) + page_footer)
+        else:
+            self.redirect("/welcome?username={}&email={}".format(user_esc, email))
 
 
 class Welcome(webapp2.RequestHandler):
     """Handles requests coming into '/welcome'."""
-    def post(self):
-        user = self.request.get(user_input)
-        email = self.request.get(email_input)
-        welcome = "Hello %s, thank you for signing up! We've added your email address as %s. Thank you!" % (user, email)
+    def get(self):
+        user = self.request.get("username")
+        email = self.request.get("email")
+        user = "<strong>" + user + "</strong>"
+        email = "<strong>" + email + "</strong>"
+        welcome = "Hello %s, thank you for signing up!" % user
+        message = "Watch for your welcome email soon!"
         welcome_element = "<p class='welcome'>" + welcome + "</p>"
-        welcome_content = page_header + welcome_element + page_footer
-        self.response.write(content)
+        message_element = "<p class='msg'>" + message + "</p><p>Email:  " + email
+        welcome_content = page_header_welcome + welcome_element + message_element + page_footer
+        self.response.write(welcome_content)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('/sign-up', SignUp),
     ('/welcome', Welcome)
     ])
